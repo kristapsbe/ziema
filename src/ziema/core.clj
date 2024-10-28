31493d8a7ed4996438c1f686e97bac4f4ec5096b
Kristaps <kbebris+git@gmail.com>
2024-10-28 21:42:37 +0200

    basic routing

--- src/ziema/core.clj
+++ src/ziema/core.clj
@@ -1,16 +1,18 @@
 ; https://medium.com/@alekcz/http-kit-compojure-and-your-first-clojure-server-1df1b0c9da3d
-(ns ziema.core  ;(1)
+(ns ziema.core
   (:gen-class)
-  (:require [org.httpkit.server :as server])) ;(2)
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn baby-small-app [req] ;(3)
+(defn baby-small-app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Pew pew!"})
 
-(defn -main ;(4)
+(defn -main
   "This is our app's entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] ;(5) 
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
   (server/run-server #'baby-small-app {:port port})
   (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
