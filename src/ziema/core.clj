fa299179e558ade5e4290a2fc4c6e512f88779ff
Kristaps <kbebris+git@gmail.com>
2024-10-28 21:58:08 +0200

    added demo code

--- src/ziema/core.clj
+++ src/ziema/core.clj
@@ -5,14 +5,33 @@
             [compojure.core :refer :all]
             [compojure.route :as route]))
 
-(defn baby-small-app [req]
+(defn fps-handler [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "Pew pew!"})
+   :body    "Pew pew!"}) 
+             
+(defn mail-man []
+  "{\"Spongebob Narrator\": \"5 years later...\"}")
 
-(defn -main
+(defn mail-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (mail-man)}) 
+                                                                                                                           
+(defn general-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "All hail General Zod!"}) 
+             
+(defroutes app-routes
+  (GET "/" [] fps-handler)
+  (POST "/postoffice" [] mail-handler)
+  (ANY "/anything-goes" [] general-handler)
+  (route/not-found "You Must Be New Here"))
+                                                                                 
+(defn -main 
   "This is our app's entry point"
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
-  (server/run-server #'baby-small-app {:port port})
+  (server/run-server #'app-routes {:port port})
   (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
